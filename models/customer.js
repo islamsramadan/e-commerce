2bb984d4a79feb80a89c12b3b6218dc05eaae72a
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-06 11:35:36 +0200

    Fix customer schema

--- models/customer.js
+++ models/customer.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
-const nameSchema = require('./common')
+const { nameSchema } = require("./common");
 
 const productsSchema = mongoose.Schema(
   {
     productId: {
-      type: String,
-      required: false
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "product",
     },
     quantity: {
       type: Number,
-      required: false
+      required: true,
     },
   },
   { _id: false }
@@ -18,14 +19,16 @@ const productsSchema = mongoose.Schema(
 
 const cartSchema = mongoose.Schema(
   {
-    products: [{
-      type: productsSchema,
-      required: false
-    }],
+    products: [
+      {
+        type: productsSchema,
+        required: false,
+      },
+    ],
     totalPrice: {
       type: Number,
       default: 0,
-      required: false
+      required: false,
     },
   },
   { _id: false }
@@ -35,20 +38,20 @@ const customerSchema = mongoose.Schema({
   _id: {
     type: mongoose.Types.ObjectId,
     required: true,
-    auto: true
+    auto: true,
   },
   userId: {
     type: mongoose.Types.ObjectId,
     ref: "user",
-    required: true
+    required: true,
   },
   name: {
     type: nameSchema,
-    required: true
+    required: true,
   },
   cart: {
     type: cartSchema,
-    required: true
+    default: { products: [], totalPrice: 0 },
   },
 });
 
