7adead1c4826486de8f9e8ca2132dcfc84fc8cc2
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-31 12:28:18 +0200

    Finish search products endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,17 @@
-require('../models/product');
+require("../models/product");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-let Products = mongoose.model('product');
-const fs = require('fs');
-const path = require('path');
-const asyncHandler = require('express-async-handler');
+let Products = mongoose.model("product");
+const fs = require("fs");
+const path = require("path");
+const asyncHandler = require("express-async-handler");
+const { match } = require("assert");
 
 module.exports.getAllProducts = (req, res, next) => {
   Products.find({})
     .then((data) => {
-      res.status(200).json({ status: 'success', data });
+      res.status(200).json({ status: "success", data });
     })
     .catch((error) => next(error));
 };
@@ -28,15 +29,15 @@ module.exports.addProduct = (req, res, next) => {
   newProduct
     .save()
     .then((data) => {
-      res.status(201).json({ status: 'success', message: 'product added' });
+      res.status(201).json({ status: "success", message: "product added" });
     })
     .catch((err) => next(err));
 };
 
 module.exports.updateProduct = (req, res, next) => {
   if (
-    req.role === 'admin' ||
-    (req.role === 'business' && req.id == req.body.id)
+    req.role === "admin" ||
+    (req.role === "business" && req.id == req.body.id)
   ) {
     Products.findOne({ _id: req.body.id })
       .then((data) => {
@@ -45,8 +46,8 @@ module.exports.updateProduct = (req, res, next) => {
         }
         return data.save().then((data) => {
           res.status(200).json({
-            status: 'success',
-            message: 'product updated',
+            status: "success",
+            message: "product updated",
           });
         });
       })
@@ -57,19 +58,19 @@ module.exports.updateProduct = (req, res, next) => {
 module.exports.getOneProduct = (req, res, next) => {
   Products.findOne({ _id: req.params.id })
     .then((data) => {
-      res.status(200).json({ status: 'success', data });
+      res.status(200).json({ status: "success", data });
     })
     .catch((err) => next(err));
 };
 
 module.exports.deleteOneProduct = (req, res, next) => {
   if (
-    req.role === 'admin' ||
-    (req.role === 'business' && req.id == req.params.id)
+    req.role === "admin" ||
+    (req.role === "business" && req.id == req.params.id)
   ) {
     Products.deleteOne({ _id: req.params.id })
       .then((data) => {
-        res.status(200).json({ status: 'success', message: 'product deleted' });
+        res.status(200).json({ status: "success", message: "product deleted" });
       })
       .catch((err) => next(err));
   }
@@ -83,7 +84,7 @@ module.exports.checkForBusinessValidity = async (req, res, next) => {
   } catch (err) {
     res.status(404).json({
       sucess: false,
-      message: 'something went wrong, please try again',
+      message: "something went wrong, please try again",
     });
   }
 };
@@ -91,12 +92,12 @@ module.exports.checkForBusinessValidity = async (req, res, next) => {
 module.exports.deleteimage = (req, res, next) => {
   const productId = req.params.id;
   const imagePath = path.join(
-    'images',
-    'products',
+    "images",
+    "products",
     productId,
     req.body.imageName
   );
-  fs.unlinkSync(imagePath, (err) => console.log('error : ', err));
+  fs.unlinkSync(imagePath, (err) => console.log("error : ", err));
 };
 
 module.exports.createProductReview = asyncHandler(async (req, res, next) => {
@@ -111,7 +112,7 @@ module.exports.createProductReview = asyncHandler(async (req, res, next) => {
 
     if (alreadyReviewed) {
       res.status(400);
-      throw new Error('Product already reviewed');
+      throw new Error("Product already reviewed");
     }
     console.log(req.id);
     const review = {
@@ -130,9 +131,32 @@ module.exports.createProductReview = asyncHandler(async (req, res, next) => {
       product.reviews.length;
 
     await product.save();
-    res.status(201).json({ message: 'Review added' });
+    res.status(201).json({ message: "Review added" });
   } else {
     res.status(404);
-    throw new Error('Product not found');
+    throw new Error("Product not found");
   }
 });
+
+exports.searchProduct = async (req, res, next) => {
+  try {
+    const searchQuery = req.query.q.trim();
+    if (!searchQuery) throw new Error("Search Query must not be empty.");
+
+    const matchingProducts = await Products.find(
+      {
+        name: { $regex: new RegExp(searchQuery, "i") },
+      },
+      { name: 1, price: 1, rating: 1 }
+    );
+
+    if (!matchingProducts.length)
+      throw new Error("Sorry! Couldnt find a product with that name");
+
+    res.json({ msg: "success", matchingProducts });
+  } catch (error) {
+    if (error.message.includes("empty")) error.status = 400;
+    if (error.message.includes("Couldnt find")) error.status = 404;
+    next(error);
+  }
+};
