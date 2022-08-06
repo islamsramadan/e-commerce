9124d374154835225bc6b0cd1b4ee13d1044f7a1
Ahmed-Basher1 <a7medbasher811@gmail.com>
2022-08-06 20:32:20 +0200

    get customer data

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,5 +5,6 @@ const adminController = require('../controllers/adminController');
 router.get('/product-count', adminController.getNumberOfProduct);
 router.get('/users-count', adminController.getNumberOfUsers);
 router.get('/order-count', adminController.getTotalOrders);
+router.get('/customer-data/:id', adminController.getCustomerData);
 
 module.exports = router;
